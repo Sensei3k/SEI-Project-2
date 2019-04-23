0ad9edbe56d867c905ed20101c897eb71d413575
Kristian Stafford Smith <kristianss7890@gmail.com>
2019-04-23 18:37:43 +0100

    LeagueTable component complete

--- src/components/LeagueTable.js
+++ src/components/LeagueTable.js
@@ -5,28 +5,77 @@ class LeagueTable extends React.Component {
     super(props)
 
     this.state ={
+      classLoaded: false,
+      leagueData: [],
       code: props.location.state.code
     }
 
   }
 
-  // componentDidMount() {
-  //   fetch('http://api.football-data.org/v2/competitions/', {
-  //     method: 'GET', //get request
-  //     headers: {
-  //       'X-Auth-Token': '4c4008915f684e03bb77b3e11617f599'
-  //     }
-  //   })
-  //     .then(res => res.json())
-  //     .then(data => this.setState({ footballData: data.competitions }))
-  //     .then(() => this.setState({ classLoaded: true }))
-  // }
+  componentDidMount() {
+    fetch(`http://api.football-data.org/v2/competitions/${this.state.code}/standings`, {
+      method: 'GET', //get request
+      headers: {
+        'X-Auth-Token': '4c4008915f684e03bb77b3e11617f599'
+      }
+    })
+      .then(res => res.json())
+      .then(data => this.setState({ leagueData: data }))
+      .then(() => this.setState({ classLoaded: true }))
+  }
 
   render() {
     console.log(this.state.code)
-    
+    console.log()
+
     return (
-      <div className="content">League Table</div>
+      <section className="section">
+        <div className="container">
+          <table className="table">
+            <thead>
+              <tr>
+                <th><abbr title="Position">Pos</abbr></th>
+                <th>Team</th>
+                <th><abbr title="Played">Pld</abbr></th>
+                <th><abbr title="Won">W</abbr></th>
+                <th><abbr title="Drawn">D</abbr></th>
+                <th><abbr title="Lost">L</abbr></th>
+                <th><abbr title="Goals for">GF</abbr></th>
+                <th><abbr title="Goals against">GA</abbr></th>
+                <th><abbr title="Goal difference">GD</abbr></th>
+                <th><abbr title="Points">Pts</abbr></th>
+              </tr>
+            </thead>
+          </table>
+          {this.state.classLoaded && this.state.leagueData.standings[0].table.map(club => {
+
+            return (
+              <div key={club.team.id} className="column is-multiline is-one-quarter-desktop is-one-third-tablet">
+                <table className="table">
+                  <tbody>
+                    <tr>
+                      <th>{club.position}</th>
+                      <td><img src={club.team.crestUrl} alt={club.team.name} className="image is-64x64" /> {club.team.name} <strong>(C)</strong></td>
+                      <td>{club.playedGames}</td>
+                      <td>{club.won}</td>
+                      <td>{club.draw}</td>
+                      <td>{club.lost}</td>
+                      <td>{club.goalsFor}</td>
+                      <td>{club.goalsAgainst}</td>
+                      <td>{club.goalDifference}</td>
+                      <td>{club.points}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            )
+          }
+
+          )}
+
+        </div>
+
+      </section>
     )
   }
 
