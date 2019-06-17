fe557f330be0bade5fac2486098863d3e149e2d4
Kristian Stafford Smith <kristianss7890@gmail.com>
2019-06-17 21:26:51 +0100

    reduced competition count, reday to style

--- src/components/Competitions.js
+++ src/components/Competitions.js
@@ -9,25 +9,25 @@ class Competitions extends React.Component {
 
     this.state = {
       footballData: [],
-      classLoaded: false,
-      //array of id's for the 7 leagues we are interested in and their corresponding images (not given with API)
-      sevenComps: [2021, 2002, 2019, 2014, 2017, 2003, 2015],
+      classLoaded: false
+    }
+
+    //array of id's for the 5 leagues we are interested in and their corresponding images (not given with API, had to be hard-coded)
+    this.competitions = {
+      sevenComps: [2021, 2002, 2019, 2014, 2015],
       sevenCompsImages: {
         2021: '../images/premier_league-logo.jpg',
         2002: '../images/bundesliga_logo_.jpg',
         2019: '../images/serie_a.jpg',
         2014: '../images/logo-laliga_280.jpg',
-        2017: '../images/primeira-liga-logo.jpg',
-        2003: '../images/eredivisie-logo-football.jpg',
         2015: '../images/ligue_1.jpg'
       }
     }
-
   }
 
   componentDidMount() {
     fetch('https://api.football-data.org/v2/competitions/', {
-      method: 'GET', //get request
+      method: 'GET',
       headers: {
         'X-Auth-Token': '4c4008915f684e03bb77b3e11617f599'
       }
@@ -45,11 +45,11 @@ class Competitions extends React.Component {
         <div className="container">
           <div className="columns is-multiline">
             {this.state.classLoaded && this.state.footballData.map(competition => {
-              if (this.state.sevenComps.includes(competition.id)) {
+              if(this.competitions.sevenComps.includes(competition.id)) {
                 return (
                   <div key={competition.id} className="column is-multiline is-one-quarter-desktop is-one-third-tablet">
                     <CompetitionCard
-                      {...competition} images={this.state.sevenCompsImages}
+                      {...competition} images={this.competitions.sevenCompsImages}
                     />
                   </div>
                 )
