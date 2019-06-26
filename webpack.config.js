84b05d1b988d95eec392ab33124b1e681326a072
Detola Oyawoye <oyawoye.ade@gmail.com>
2019-06-26 19:48:58 +0100

    added webpack script for local images

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,16 @@ module.exports = {
       { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
       { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.(jpe?g|png|gif|svg)$/, loader: 'file-loader' }
+      { test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: './images/[name].[ext]'
+            }
+          }
+        ]
+      }
     ]
   },
   devServer: {
