70e88685f6c7de48397ede7ad30592d90dbc6cce
Detola Oyawoye <oyawoye.ade@gmail.com>
2019-06-27 16:59:39 +0100

    webpack.config.js replaced and images in competition path changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,21 @@
 const path = require('path')
 const webpack = require('webpack')
-const dotenv = require('dotenv-webpack')
-
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
-const env = dotenv.config().parsed
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next])
-  return prev
-}, {})
+const EnvPlugin = process.env.NODE_ENV === 'production' ? (
+  new webpack.EnvironmentPlugin({ ...process.env })
+) : (
+  new Dotenv()
+)
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'app.js'
+    filename: 'bundle.js'
   },
   devtool: 'source-map',
   module: {
@@ -24,15 +23,23 @@ module.exports = {
       { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
       { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.(jpe?g|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: './images/[name].[ext]'
-            }
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: './images/background-imgaes/[name].[ext]'
           }
-        ]
+        }
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: './fonts/[name].[ext]'
+          }
+        }
       }
     ]
   },
@@ -40,7 +47,12 @@ module.exports = {
     hot: true,
     inline: true,
     port: 8000,
-    open: true
+    open: true,
+    contentBase: 'src',
+    watchContentBase: true,
+    proxy: {
+      '/api': 'http://localhost:4000'
+    }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -50,8 +62,8 @@ module.exports = {
       inject: 'body'
     }),
     new CopyWebpackPlugin([
-      { from: 'src/images', to: 'images' }
+      { from: 'src/images', to: 'images'}
     ]),
-    new webpack.DefinePlugin(envKeys)
+    EnvPlugin
   ]
 }
