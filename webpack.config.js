fd3e9e4c93be2854e90bc8bc73df3a6b10f1182d
Detola Oyawoye <oyawoye.ade@gmail.com>
2019-06-27 11:58:30 +0100

    competiton images fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv-webpack')
 
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
